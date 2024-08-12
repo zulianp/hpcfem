e2227bc42250c114ed310e339b3f595127fd331e
Bole Ma <b.ma@tum.de>
2024-08-12 18:21:29 +0200

    Fixed a bug in inverse_3x3

--- a64fx/macro.cpp
+++ a64fx/macro.cpp
@@ -61,7 +61,6 @@ real_t determinant_3x3(real_t *m) {
 real_t inverse_3x3(real_t *m, real_t *m_inv)
 {
     real_t det_inv = 1.0 / determinant_3x3(m);
-    real_t m_inv; // inverse of matrix m
 
     m_inv[0*3+0] = (m[1*3+1] * m[2*3+2] - m[2*3+1] * m[1*3+2]) * det_inv;
     m_inv[0*3+1] = (m[0*3+2] * m[2*3+1] - m[0*3+1] * m[2*3+2]) * det_inv;
